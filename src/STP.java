8d6a16b2c610d179124f96e04f9d058c17b70d4c
tobias.sandstrom1 <tobias.sandstrom@elev.ga.ntig.se>
2020-05-28 10:25:32 +0200

    add PvE

---
+++ src/STP.java
@@ -0,0 +1,65 @@
+import javax.swing.*;
+import java.util.Random;
+
+public class STP {
+    public static void main(String[] args) {
+        int antalSpelare, dator, spelare1, spelare2;
+        int poang1 = 0;
+        int poang2 = 0;
+        int datorpoang = 0;
+        int rundor = 3;
+        Random rand = new Random();
+
+
+        antalSpelare = Integer.parseInt(JOptionPane.showInputDialog(null, "Välj antal spelare (1) eller (2)"));
+
+        while (antalSpelare > 2 || antalSpelare < 1 ) {
+            antalSpelare = Integer.parseInt(JOptionPane.showInputDialog(null, "Du måste välja antingen (1) eller (2)"));
+        }
+
+        while (rundor > 0) {
+
+        if (antalSpelare == 1) {
+            spelare1 = Integer.parseInt(JOptionPane.showInputDialog(null,"(Bara nummer!) Välj Sten(1) Sax(2) eller Påse(3)"));
+            dator = rand.nextInt(3)+1;
+
+            if (spelare1 == dator) {
+                JOptionPane.showMessageDialog(null,"Ni båda valde samma sak");
+                rundor++;
+            }
+            else if (spelare1 == 1 && dator == 2) {
+                JOptionPane.showMessageDialog(null,"Spelare 1 valde sten och dator valde sax!" + "\n" + "Spelare 1 vinner!");
+                poang1++;
+            }
+            else if (spelare1 == 1 && dator == 3) {
+                JOptionPane.showMessageDialog(null,"Spelare 1 valde sten och dator valde påse" + "\n" + "Dator vinner!");
+                datorpoang++;
+            }
+            else if (spelare1 == 2 && dator == 3) {
+                JOptionPane.showMessageDialog(null,"Spelare 1 valde sten och dator valde sax!" + "\n" + "Spelare 1 vinner!");
+                poang1++;
+            }
+            else if (spelare1 == 2 && dator == 1) {
+                JOptionPane.showMessageDialog(null,"Spelare 1 valde sten och dator valde påse" + "\n" + "Dator vinner!");
+                datorpoang++;
+            }
+            else if (spelare1 == 3 && dator == 1) {
+                JOptionPane.showMessageDialog(null,"Spelare 1 valde sten och dator valde sax!" + "\n" + "Spelare 1 vinner!");
+                poang1++;
+            }
+            else if (spelare1 == 3 && dator == 2) {
+                JOptionPane.showMessageDialog(null,"Spelare 1 valde sten och dator valde påse" + "\n" + "Dator vinner!");
+                datorpoang++;
+            }
+            rundor--;
+            JOptionPane.showMessageDialog(null, "Resultat: " + "Spelare 1 har " + poang1 + " poäng och datorn har " + datorpoang + " poäng!" + "\n" + rundor + " Rundor kvar!" );
+            if (rundor == 0 && poang1 > datorpoang) {
+                JOptionPane.showMessageDialog(null, "Spelare 1 vinner hela spelet!");
+            }
+                else if (rundor == 0 && datorpoang > poang1) {
+                    JOptionPane.showMessageDialog(null, "Datorn vinner hela spelet!");
+                }
+            }
+        }
+    }
+}
